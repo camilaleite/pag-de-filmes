438e8ff74e9dbb066479b3540cb6efef9f035ed9
camilaleite <93920974+camilaleite@users.noreply.github.com>
2022-05-07 16:33:48 -0300

    colocando-destaque

--- src/DestaqueMovie.js
+++ src/DestaqueMovie.js
@@ -2,5 +2,13 @@ import React from "react";
 import "./DestaqueMovie.css";
 
 export default function ({ item }) {
-  return <section className="Destaques">RED</section>;
+  return (
+    <section className="Destaques">
+      RED
+      <img
+        class="red"
+        src="https://disneyplusbrasil.com.br/wp-content/uploads/2021/06/Turning-Red-750x422.jpg"
+      />
+    </section>
+  );
 }
