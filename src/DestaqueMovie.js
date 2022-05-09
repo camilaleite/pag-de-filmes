2f8fb9dbab3b801bba00bde85f6edcaca9042e56
camilaleite <93920974+camilaleite@users.noreply.github.com>
2022-05-09 16:16:44 -0300

    atualizandoheader

--- src/DestaqueMovie.js
+++ src/DestaqueMovie.js
@@ -1,14 +1,24 @@
 import React from "react";
 import "./DestaqueMovie.css";
 
-export default function ({ item }) {
+export default function () {
   return (
     <section className="Destaques">
-      RED
+      <h1> RED - Crescer é uma fera</h1>
       <img
         class="red"
         src="https://disneyplusbrasil.com.br/wp-content/uploads/2021/06/Turning-Red-750x422.jpg"
       />
+      <p>
+        Novo filme da Disney e Pixar, chegou quebrando recordes no Disney+, se
+        tornando a produção com a melhor performance mundial no dia da estreia
+        desde o lançamento do streaming em 2018. A animação conta a história de
+        Mei Lee, uma garota de 13 anos dividida entre continuar sendo uma filha
+        obediente e o caos da adolescência. Como se as mudanças em seus
+        interesses, relações e em seu corpo não bastassem, sempre que passa por
+        fortes emoções (o que acontece praticamente sempre), ela se transforma
+        em um panda vermelho gigante.
+      </p>
     </section>
   );
 }
